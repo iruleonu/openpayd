2ceed9772efa07680dc9e6ed86bd17db363d4dc8
Nuno Salvador <nuno@salvador.com>
2019-06-03 00:45:53 +0100

    Removed old stuff.

--- IRShowcase/Common/Persistence/PersistenceLayer.swift
+++ IRShowcase/Common/Persistence/PersistenceLayer.swift
@@ -14,19 +14,6 @@ import IRCoreDataStack
 
 typealias PersistenceSaveCompletion = (Bool, Error?) -> Void
 
-protocol EntityFetchPostsProtocol {
-    func fetchAllPosts() -> SignalProducer<[Post], PersistenceLayerError>
-    func fetchPost(postId: Int64) -> SignalProducer<[Post], PersistenceLayerError>
-}
-
-protocol EntityFetchCommentsProtocol {
-    func fetchComments(postId: Int64) -> SignalProducer<[Comment], PersistenceLayerError>
-}
-
-protocol EntityFetchUsersProtocol {
-    func fetchUser(userId: Int64) -> SignalProducer<[User], PersistenceLayerError>
-}
-
 protocol PersistenceLayerLoad {
     func fetchResource<T>(_ resource: Resource) -> SignalProducer<T, PersistenceLayerError>
 }
@@ -68,12 +55,6 @@ extension PersistenceLayerImpl: PersistenceLayerLoad {
         case .post(let postId):
             guard let pId = Int64(postId) else { return SignalProducer.empty }
             return fetchPost(postId: pId) as? SignalProducer<T, PersistenceLayerError> ?? SignalProducer.empty
-        case .comments(let postId):
-            guard let pId = Int64(postId) else { return SignalProducer.empty }
-            return fetchComments(postId: pId) as? SignalProducer<T, PersistenceLayerError> ?? SignalProducer.empty
-        case .user(let userId):
-            guard let uId = Int64(userId) else { return SignalProducer.empty }
-            return fetchUser(userId: uId) as? SignalProducer<T, PersistenceLayerError> ?? SignalProducer.empty
         default:
             return SignalProducer<T, PersistenceLayerError> { observer, _ in
                 observer.send(error: PersistenceLayerError.persistence(error: NSError.error(withMessage: "No known resource")))
@@ -93,10 +74,6 @@ extension PersistenceLayerImpl: PersistenceLayerRemove {
             return deleteAllEntities(entityName: "PostCD")
         case .post(let postId):
             return deleteEntity(entityName: "PostCD", entityIdenfitier: postId)
-        case .comments(let postId):
-            return deleteEntity(entityName: "CommentCD", entityIdenfitier: postId)
-        case .user(let userId):
-            return deleteEntity(entityName: "UserCD", entityIdenfitier: userId)
         default:
             return SignalProducer<Bool, PersistenceLayerError> { observer, _ in
                 observer.send(error: PersistenceLayerError.persistence(error: NSError.error(withMessage: "Unknown resource")))
@@ -196,116 +173,5 @@ extension PersistenceLayerImpl: PersistenceLayerSave {
 extension PersistenceLayerImpl: AppService {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
-    }
-}
-
-extension PersistenceLayerImpl: EntityFetchPostsProtocol {
-    func fetchAllPosts() -> SignalProducer<[Post], PersistenceLayerError> {
-        let context = coreDataStack.managedObjectContext!
-
-        return SignalProducer<[Post], PersistenceLayerError> { observer, _ in
-            var results: [Post] = []
-
-            do {
-                let fetchRequest: NSFetchRequest<PostCD> = PostCD.fetchRequest()
-                let aux = try context.fetch(fetchRequest) as [PostCD]
-                aux.forEach({ (postCD) in
-                    guard let post: Post = Post.mapToModel(postCD) else { return }
-                    results.append(post)
-                })
-            } catch {}
-            
-            guard results.count > 0 else {
-                let error = NSError.error(withMessage: "No results when trying to fetch all posts")
-                observer.send(error: PersistenceLayerError.emptyResult(error: error))
-                return
-            }
-            
-            observer.send(value: results)
-        }
-    }
-    
-    func fetchPost(postId: Int64) -> SignalProducer<[Post], PersistenceLayerError> {
-        let context = coreDataStack.managedObjectContext!
-        
-        return SignalProducer<[Post], PersistenceLayerError> { observer, _ in
-            var results: [Post] = []
-            
-            do {
-                let fetchRequest: NSFetchRequest<PostCD> = PostCD.fetchRequest()
-                fetchRequest.predicate = NSPredicate(format: "id == %lld", Int64(postId))
-                let aux = try context.fetch(fetchRequest) as [PostCD]
-                aux.forEach({ (postCD) in
-                    guard let post: Post = Post.mapToModel(postCD) else { return }
-                    results.append(post)
-                })
-            } catch {}
-            
-            guard results.count > 0 else {
-                let error = NSError.error(withMessage: "No results for post with id: \(postId)")
-                observer.send(error: PersistenceLayerError.emptyResult(error: error))
-                return
-            }
-            
-            observer.send(value: results)
-        }
-    }
-}
-
-extension PersistenceLayerImpl: EntityFetchUsersProtocol {
-    func fetchUser(userId: Int64) -> SignalProducer<[User], PersistenceLayerError> {
-        let context = coreDataStack.managedObjectContext!
-        
-        return SignalProducer<[User], PersistenceLayerError> { observer, _ in
-            var results: [User] = []
-            
-            do {
-                let fetchRequest: NSFetchRequest<UserCD> = UserCD.fetchRequest()
-                fetchRequest.predicate = NSPredicate(format: "id == %lld", Int64(userId))
-                let aux = try context.fetch(fetchRequest) as [UserCD]
-                aux.forEach({ (a) in
-                    guard let user: User = User.mapToModel(a) else { return }
-                    results.append(user)
-                })
-            } catch {}
-            
-            guard results.count > 0 else {
-                let error = NSError.error(withMessage: "No results for user with id: \(userId)")
-                observer.send(error: PersistenceLayerError.emptyResult(error: error))
-                return
-            }
-            
-            observer.send(value: results)
-        }
-    }
-}
-
-extension PersistenceLayerImpl: EntityFetchCommentsProtocol {
-    func fetchComments(postId: Int64) -> SignalProducer<[Comment], PersistenceLayerError> {
-        let context = coreDataStack.managedObjectContext!
-        
-        return SignalProducer<[Comment], PersistenceLayerError> { observer, _ in
-            var results: [Comment] = []
-            
-            do {
-                let fetchRequest: NSFetchRequest<CommentCD> = CommentCD.fetchRequest()
-                fetchRequest.predicate = NSPredicate(format: "postId == %d", Int64(postId))
-                let aux = try context.fetch(fetchRequest) as [CommentCD]
-                aux.forEach({ (a) in
-                    guard let comment: Comment = Comment.mapToModel(a) else {
-                        return
-                    }
-                    results.append(comment)
-                })
-            } catch {}
-            
-            guard results.count > 0 else {
-                let error = NSError.error(withMessage: "Comment for post id: \(postId) not found")
-                observer.send(error: PersistenceLayerError.emptyResult(error: error))
-                return
-            }
-            
-            observer.send(value: results)
-        }
     }
 }
